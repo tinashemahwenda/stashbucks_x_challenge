714aec66ba6acf3c98c857cc5dbedcdcd51e1c2b
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-08 10:23:06 +0200

    Set the value for the constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stashbucks_x_challenge/constants/constants.dart';
 import 'package:stashbucks_x_challenge/screens/home_page.dart';
 
 void main() {
@@ -10,8 +11,16 @@ class StashBucksApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
-    );
+    return Builder(builder: (context) {
+      AppSize.width = MediaQuery.of(context).size.width;
+      AppSize.height = MediaQuery.of(context).size.height;
+
+      AppColor.textColor = Color.fromARGB(255, 34, 51, 114);
+      AppColor.highlightColor = Color.fromARGB(255, 21, 161, 254);
+      AppColor.secondaryText = Color.fromARGB(255, 15, 24, 57);
+      return MaterialApp(
+        home: HomePage(),
+      );
+    });
   }
 }
