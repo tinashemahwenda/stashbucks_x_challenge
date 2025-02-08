405c3e7f1922b07d9ac3efd8ce2ac45fceb051a5
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-08 10:10:45 +0200

    Created constants for colors

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,4 +1,13 @@
+import 'package:flutter/material.dart';
+
 class AppSize {
   static late double width;
   static late double height;
+}
+
+class AppColor {
+  static late Color textColor;
+  static late Color buttonColor;
+  static late Color secondaryText;
+  static late Color highlightColor;
 }
