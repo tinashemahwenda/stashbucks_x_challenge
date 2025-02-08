33d519c80adb61fcf902576c376eb5d15941135d
Tinashe Mahwenda <40965708+tinashemahwenda@users.noreply.github.com>
2025-02-08 10:28:46 +0200

    Created the navbar, which is the row

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:stashbucks_x_challenge/constants/constants.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Login Page'),
-    );
+    return SafeArea(
+        child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(
+                Icons.cancel,
+                color: AppColor.highlightColor,
+                size: 30,
+              ),
+              Text('Welcome to Stashbucks'),
+            ],
+          )
+        ],
+      ),
+    ));
   }
 }
